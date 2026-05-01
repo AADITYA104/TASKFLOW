d4489e7ad9059cb4c53e5975e2a58fb0f32c7a4f
AADITYA104 <devmurariaaditya@gmail.com>
2026-05-01 16:18:18 +0530

    Clean up unused temporary files

--- read_pdf.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pypdf import PdfReader
-
-reader = PdfReader('../TASKFLOW_Analysis_Report.pdf')
-text = ""
-for page in reader.pages:
-    text += page.extract_text() + "\n"
-with open('report.txt', 'w', encoding='utf-8') as f:
-    f.write(text)
